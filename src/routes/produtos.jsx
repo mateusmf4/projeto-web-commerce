4c1f91f3af61ce5a2221cdec7480dace30b5a9ec
Mateus <130506942+mateusmf4@users.noreply.github.com>
2025-09-05 19:44:38 -0300

    feat: versão inicial da pagina de produtos

--- src/routes/produtos.jsx
+++ src/routes/produtos.jsx
@@ -1,8 +1,43 @@
+import { SearchIcon } from "lucide-react";
+import "./produtos.css";
+
+function ProdutoCard() {
+  return (
+    <a href="/produtos" className="produtoCard">
+      <img src="https://api.dicebear.com/9.x/shapes/svg" alt="produto" />
+      <h3 class="produtoCard--nome">
+        Nome do produto 100g DeMarca Barata sabor Pimenta
+      </h3>
+      <p class="produtoCard--preco">R$ 25,50</p>
+    </a>
+  );
+}
+
 export default function Produtos() {
   return (
     <main>
-      <h1>Pagina de produtos</h1>
-      <a href="/carrinho">ir pra carrinho</a>
+      <div className="main--categorias">
+        <h2>Categorias</h2>
+      </div>
+      <div className="main--listagem">
+        <div className="main--listagem--pesquisa">
+          <input
+            type="text"
+            name="produto-search"
+            placeholder="Digite o nome de um produto..."
+          />
+          <SearchIcon />
+        </div>
+
+        <div className="main--listagem--produtos">
+          <ProdutoCard />
+          <ProdutoCard />
+          <ProdutoCard />
+          <ProdutoCard />
+          <ProdutoCard />
+          <ProdutoCard />
+        </div>
+      </div>
     </main>
   );
 }
