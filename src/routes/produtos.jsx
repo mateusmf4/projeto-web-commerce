aab78d74b4165159f5447f8744976f2bca27f360
Mateus <130506942+mateusmf4@users.noreply.github.com>
2025-09-10 00:11:25 -0300

    feat: categorias

--- src/routes/produtos.jsx
+++ src/routes/produtos.jsx
@@ -1,12 +1,33 @@
 import { SearchIcon } from "lucide-react";
 import "./produtos.css";
+import { ChevronRightIcon } from "lucide-react";
+
+function Categoria({ nome }) {
+  const tags = ["Electrolux", "SONY", "Nvidia", "Logitech", "Panasonic"];
+  return (
+    <details className="categoria" name="categoria-produtos">
+      <summary>
+        <ChevronRightIcon size="1em" className="categoria__icon" />
+        {nome}
+      </summary>
+      <div className="categoria__tags">
+        {tags.map((tag) => (
+          <label key={tag}>
+            <input type="checkbox" />
+            <span>{tag}</span>
+          </label>
+        ))}
+      </div>
+    </details>
+  );
+}
 
 function ProdutoCard() {
   return (
     <a href="/produtos" className="produtoCard">
       <img src="https://api.dicebear.com/9.x/shapes/svg?seed=2" alt="produto" />
       <h3 className="produtoCard__nome">
-        Nome do produto 100g DeMarca Barata sabor Pimenta
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
       </h3>
       <p className="produtoCard__preco">R$ 25,50</p>
     </a>
@@ -16,8 +37,14 @@ function ProdutoCard() {
 export default function Produtos() {
   return (
     <main>
-      <div className="main__categorias">
+      <div className="main__sidebar">
         <h2>Categorias</h2>
+
+        <div className="main__sidebar__categorias">
+          <Categoria nome="Eletrodomésticos" />
+          <Categoria nome="Informática" />
+          <Categoria nome="TV e Video" />
+        </div>
       </div>
       <div className="main__listagem">
         <div className="main__listagem__pesquisa">
