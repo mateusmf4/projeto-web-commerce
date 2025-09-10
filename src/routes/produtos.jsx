fa3d88be3f85bfa3d9514e4e135bf38498bb24fe
Mateus <130506942+mateusmf4@users.noreply.github.com>
2025-09-10 17:21:53 -0300

    feat: categorias e tags, no mobile tbm

--- src/routes/produtos.jsx
+++ src/routes/produtos.jsx
@@ -1,24 +1,83 @@
 import { SearchIcon } from "lucide-react";
 import "./produtos.css";
-import { ChevronRightIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon } from "lucide-react";
+import { useMemo, useRef, useState } from "react";
 
-function Categoria({ nome }) {
-  const tags = ["Electrolux", "SONY", "Nvidia", "Logitech", "Panasonic"];
+function Categoria({
+  nome,
+  tags,
+  tagsAtivas,
+  active,
+  mudarCategoria,
+  toggleTag,
+  resetarTags,
+}) {
+  const toggleDetails = (e) => {
+    if (e.target.open) {
+      mudarCategoria(nome);
+    }
+  };
   return (
-    <details className="categoria" name="categoria-produtos">
-      <summary>
+    <details
+      className="categoria"
+      name="categoria-produtos"
+      onToggle={toggleDetails}
+      open={active}
+    >
+      <summary data-active={active}>
         <ChevronRightIcon size="1em" className="categoria__icon" />
         {nome}
       </summary>
       <div className="categoria__tags">
+        <label>
+          <input
+            type="checkbox"
+            checked={tagsAtivas.length === 0}
+            onChange={(e) => e.target.checked && resetarTags()}
+          />
+          <span>Todos</span>
+        </label>
         {tags.map((tag) => (
           <label key={tag}>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={tagsAtivas.includes(tag)}
+              onChange={(e) => toggleTag(tag, e.target.checked)}
+            />
             <span>{tag}</span>
           </label>
         ))}
       </div>
     </details>
+  );
+}
+
+function TagMobile({ tag, active, onClick }) {
+  return (
+    <button type="button" onClick={onClick} data-active={!!active}>
+      {!!active && <CheckIcon size="1em" />}
+      {tag}
+    </button>
+  );
+}
+
+function TagsMobile({ tags, tagsAtivas, toggleTag, resetarTags }) {
+  const todos = useMemo(() => {
+    return tagsAtivas.length === 0;
+  }, [tagsAtivas]);
+
+  return (
+    <div className="mobile-tags">
+      <TagMobile tag="Todos" active={todos} onClick={resetarTags} />
+      {tags.map((tag) => (
+        <TagMobile
+          key={tag}
+          tag={tag}
+          active={tagsAtivas.includes(tag)}
+          onClick={() => toggleTag(tag, !tagsAtivas.includes(tag))}
+        />
+      ))}
+    </div>
   );
 }
 
@@ -35,18 +94,98 @@ function ProdutoCard() {
 }
 
 export default function Produtos() {
+  const categorias = [
+    "Todos os produtos",
+    "Eletrodomésticos",
+    "Informática",
+    "TV e Video",
+  ];
+  const [categoriaAtiva, setCategoriaAtiva] = useState(0);
+  const [tags, _setTags] = useState([
+    "Electrolux",
+    "SONY",
+    "Nvidia",
+    "Logitech",
+    "Panasonic",
+  ]);
+  const [tagsAtivas, setTagsAtivas] = useState([]);
+
+  const mudarCategoria = (nome) => {
+    const index = categorias.indexOf(nome);
+    if (index === -1) return;
+    if (index === categoriaAtiva) return;
+    setCategoriaAtiva(index);
+    setTagsAtivas([]);
+  };
+
+  const toggleTag = (tag, value) => {
+    if (value && !tagsAtivas.includes(tag)) {
+      setTagsAtivas([...tagsAtivas, tag]);
+    } else if (!value && tagsAtivas.includes(tag)) {
+      setTagsAtivas(tagsAtivas.filter((x) => x !== tag));
+    }
+  };
+
+  const resetarTags = () => {
+    setTagsAtivas([]);
+  };
+
+  const modalCategoriaRef = useRef(null);
   return (
     <main>
       <div className="main__sidebar">
         <h2>Categorias</h2>
 
         <div className="main__sidebar__categorias">
-          <Categoria nome="Eletrodomésticos" />
-          <Categoria nome="Informática" />
-          <Categoria nome="TV e Video" />
+          {categorias.map((categoria, i) => (
+            <Categoria
+              key={categoria}
+              nome={categoria}
+              tags={tags}
+              tagsAtivas={tagsAtivas}
+              active={categoriaAtiva === i}
+              mudarCategoria={mudarCategoria}
+              toggleTag={toggleTag}
+              resetarTags={resetarTags}
+            />
+          ))}
         </div>
       </div>
       <div className="main__listagem">
+        <button
+          className="main__listagem__categoria-btn"
+          type="button"
+          onClick={() => modalCategoriaRef.current.showModal()}
+        >
+          <ChevronRightIcon strokeWidth={1.5} size="1.4em" />
+          {categorias[categoriaAtiva]}
+        </button>
+        <dialog className="modal-categoria" ref={modalCategoriaRef}>
+          <h2>Categorias</h2>
+
+          <div className="list">
+            {categorias.map((categoria, i) => (
+              <button
+                key={categoria}
+                type="button"
+                data-active={i === categoriaAtiva}
+                onClick={() => {
+                  mudarCategoria(categoria);
+                  modalCategoriaRef.current.close();
+                }}
+              >
+                {categoria}
+              </button>
+            ))}
+          </div>
+        </dialog>
+        <TagsMobile
+          tags={tags}
+          tagsAtivas={tagsAtivas}
+          toggleTag={toggleTag}
+          resetarTags={resetarTags}
+        />
+
         <div className="main__listagem__pesquisa">
           <input
             type="text"
