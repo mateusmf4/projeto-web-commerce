dca1954f90f32cd04e6ef44e8566f855b3846a04
FLuiz22 <filipeluiz39@gmail.com>
2025-09-16 18:08:44 -0300

    Feat: resumo pro checkout

--- src/routes/carrinho.jsx
+++ src/routes/carrinho.jsx
@@ -1,8 +1,41 @@
-export default function Home() {
+import { Button, Card, Col, ListGroup, Row } from 'react-bootstrap';
+import './carrinho.css';
+
+export default function Carrinho() {
   return (
     <main>
-      <h1>carrinho</h1>
-      <a href="/">Ir pra home</a>
+      <div className="carrinho">
+        <div className="carrinho__listagem">
+          <h1>Produtos</h1>
+        </div>
+
+        <div className="carrinho__checkout border p-3">
+          <Row>
+            <Col>Subtotal:</Col>
+            <Col className='text-end'>R$ 35,00</Col>
+          </Row>
+
+          <Row>
+            <Col>Frete:</Col>
+            <Col className='text-end'>R$ 35,00</Col>
+          </Row>
+
+          <Row>
+            <Col>Impostos:</Col>
+            <Col className='text-end'>R$ 35,00</Col>
+          </Row>
+
+          <hr />
+
+          <Row>
+            <Col>Total:</Col>
+            <Col className='text-end'>R$ 35,00</Col>
+          </Row>
+
+          <Button className='carrinho__btn'>Checkout</Button>
+
+        </div>
+      </div>
     </main>
   );
 }
