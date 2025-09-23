a72aedaef719dc21d4dee9245c0d3148a7c17fce
FLuiz22 <filipeluiz39@gmail.com>
2025-09-23 17:21:10 -0300

    Style: estilização da entrega

--- src/components/etapaEntrega.jsx
+++ src/components/etapaEntrega.jsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form"
+import "./etapaEntrega.css"
+import { Button } from "react-bootstrap";
 
 export default function EtapaEntrega({ dados, onNext }) {
   const { register, handleSubmit, formState: { errors }} = useForm({
@@ -12,36 +14,62 @@ export default function EtapaEntrega({ dados, onNext }) {
   return (
     <div>
       <h2>Endereço de Entrega</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          placeholder="Endereço"
-          {...register("endereco", { required: "O endereço é obrigatório" })}
-        />
-        {errors.endereco && <p style={{ color: "red" }}>{errors.endereco.message}</p>}
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <div className="form__input">
+          <div>
+            <input
+              type="text"
+              placeholder="CEP"
+              className="input__cep border rounded"
+              {...register("cep", { required: "O CEP é obrigatório" })}
+            />
+            {errors.cep && <p style={{ color: "red" }}>{errors.cep.message}</p>}
+          </div>
 
-        <input
-          type="text"
-          placeholder="Número"
-          {...register("numero", { required: "O número é obrigatório" })}
-        />
-        {errors.numero && <p style={{ color: "red" }}>{errors.numero.message}</p>}
+          <div>
+            <input
+              type="text"
+              placeholder="Logradouro"
+              className="input__logradouro border rounded"
+              {...register("logradouro", { required: "O logradouro é obrigatório" })}
+            />
+            {errors.logradouro && <p style={{ color: "red" }}>{errors.logradouro.message}</p>}
+          </div>
 
-        <input
-          type="text"
-          placeholder="Cidade"
-          {...register("cidade", { required: "A cidade é obrigatória" })}
-        />
-        {errors.cidade && <p style={{ color: "red" }}>{errors.cidade.message}</p>}
+          <div>
+            <input
+              type="text"
+              placeholder="Número"
+              className="input__numero border rounded"
+              {...register("numero", { required: "O número é obrigatório" })}
+            />
+            {errors.numero && <p style={{ color: "red" }}>{errors.numero.message}</p>}
+          </div>
+        </div>
 
-        <input
-          type="text"
-          placeholder="Bairro"
-          {...register("bairro", { required: "O bairro é obrigatório" })}
-        />
-        {errors.bairro && <p style={{ color: "red" }}>{errors.bairro.message}</p>}
+        <div className="form__input">
+          <div>
+            <input
+              type="text"
+              placeholder="Bairro"
+              className="border rounded"
+              {...register("bairro", { required: "O bairro é obrigatório" })}
+            />
+            {errors.bairro && <p style={{ color: "red" }}>{errors.bairro.message}</p>}
+          </div>
 
-        <button type="submit">Próximo</button>
+          <div>
+            <input
+              type="text"
+              placeholder="Cidade"
+              className="border rounded"
+              {...register("cidade", { required: "A cidade é obrigatória" })}
+            />
+            {errors.cidade && <p style={{ color: "red" }}>{errors.cidade.message}</p>}
+          </div>
+        </div>
+
+        <Button className="form__button" type="submit">Próximo</Button>
       </form>
     </div>
   )
