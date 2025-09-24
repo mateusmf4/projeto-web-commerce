c6db82fc1387048884155d09e22472fcd917f881
Mateus <130506942+mateusmf4@users.noreply.github.com>
2025-09-24 14:47:07 -0300

    fix: fix mobile na etapa resumo

--- src/components/etapaResumo.jsx
+++ src/components/etapaResumo.jsx
@@ -69,50 +69,49 @@ export default function EtapaResumo({ dados, onPrev, onConfirm }) {
             <Button onClick={onConfirm}>Finalizar Pedido</Button>
           </div>
         </div>
-
-        {/* Checkout no final da pagina no mobile.. */}
-        <div className="d-lg-none" style={{ height: "10rem" }}></div>
-        <div className="d-lg-none d-flex flex-column fixed-bottom bg-body p-4 border-top">
-          <Row>
-            <Col>
-              {`Endereço de entrega: ${dados.entrega.logradouro}, ${dados.entrega.numero},
+      </div>
+      {/* Checkout no final da pagina no mobile.. */}
+      <div className="d-lg-none" style={{ height: "20rem" }}></div>
+      <div className="d-lg-none d-flex flex-column fixed-bottom bg-body p-4 border-top">
+        <Row>
+          <Col>
+            {`Endereço de entrega: ${dados.entrega.logradouro}, ${dados.entrega.numero},
                 ${dados.entrega.cep}, ${dados.entrega.bairro}, ${dados.entrega.cidade}`}
-            </Col>
-          </Row>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col>{`Pagamento: ${dados.pagamento.parcela}`}</Col>
-          </Row>
+        <Row>
+          <Col>{`Pagamento: ${dados.pagamento.parcela}`}</Col>
+        </Row>
 
-          <hr />
+        <hr />
 
-          <Row>
-            <Col>Subtotal:</Col>
-            <Col className="text-end">{formatPrice(subtotal)}</Col>
-          </Row>
+        <Row>
+          <Col>Subtotal:</Col>
+          <Col className="text-end">{formatPrice(subtotal)}</Col>
+        </Row>
 
-          <Row>
-            <Col>Frete:</Col>
-            <Col className="text-end">{formatPrice(frete)}</Col>
-          </Row>
+        <Row>
+          <Col>Frete:</Col>
+          <Col className="text-end">{formatPrice(frete)}</Col>
+        </Row>
 
-          <Row>
-            <Col>Impostos:</Col>
-            <Col className="text-end">{formatPrice(impostos)}</Col>
-          </Row>
+        <Row>
+          <Col>Impostos:</Col>
+          <Col className="text-end">{formatPrice(impostos)}</Col>
+        </Row>
 
-          <hr />
+        <hr />
 
-          <Row>
-            <Col>Total:</Col>
-            <Col className="text-end">{formatPrice(total)}</Col>
-          </Row>
+        <Row>
+          <Col>Total:</Col>
+          <Col className="text-end">{formatPrice(total)}</Col>
+        </Row>
 
-          <div className="resumo-buttons">
-            <Button onClick={onPrev}>Voltar</Button>
+        <div className="resumo-buttons">
+          <Button onClick={onPrev}>Voltar</Button>
 
-            <Button onClick={onConfirm}>Finalizar Pedido</Button>
-          </div>
+          <Button onClick={onConfirm}>Finalizar Pedido</Button>
         </div>
       </div>
     </div>
